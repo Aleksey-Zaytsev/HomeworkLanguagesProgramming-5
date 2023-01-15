4af815b5643830881fa17540daa95a9a092052e2
Aleksey <a.y.zayitsev@gmail.com>
2023-01-15 21:38:17 +0300

    Сумма элементов, стоящих на нечётных позициях

---
+++ Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+Console.Write("[ ");
+for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(-100,100);
+        Console.Write($"{numbers[i]} ");
+    }
+Console.Write("]");
+Console.WriteLine(" <- Массив чисел ");
+
+int sum = 0;
+
+for (int j = 0; j < numbers.Length; j+=2)
+    sum = sum + numbers[j];
+
+Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях равна: {sum} ");
